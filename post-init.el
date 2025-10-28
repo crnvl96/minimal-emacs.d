3746ced7596dc041ed0ea6b14dc5eab2708495a0
Adran Carnavale <adran@hey.com>
2025-10-28 11:41:44 -0300

    updates

--- post-init.el
+++ post-init.el
@@ -53,8 +53,8 @@
   :ensure t
   :custom
   (completion-styles '(orderless basic))
-  (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles partial-completion)))))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
 
 (use-package marginalia
   :ensure t
