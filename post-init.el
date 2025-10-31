01597e2210f212874a53ba06a584ca3335127367
Adran Carnavale <adran@hey.com>
2025-10-31 17:59:18 -0300

    Refact

--- post-init.el
+++ post-init.el
@@ -1,13 +1,178 @@
 ;;; post-init.el --- Post Init -*- no-byte-compile: t; lexical-binding: t; -*-
 
-(minimal-emacs-load-user-init "user/compile.el")
-(minimal-emacs-load-user-init "user/emacs.el")
-(minimal-emacs-load-user-init "user/themes.el")
-(minimal-emacs-load-user-init "user/treesit.el")
-(minimal-emacs-load-user-init "user/completion.el")
-(minimal-emacs-load-user-init "user/magit.el")
-(minimal-emacs-load-user-init "user/ui.el")
-(minimal-emacs-load-user-init "user/nav.el")
+;; Compilation
+(use-package compile-angel
+  :demand t
+  :custom
+  (compile-angel-verbose t)
+  :config
+  (push "/init.el" compile-angel-excluded-files)
+  (push "/early-init.el" compile-angel-excluded-files)
+  (push "/pre-init.el" compile-angel-excluded-files)
+  (push "/post-init.el" compile-angel-excluded-files)
+  (push "/pre-early-init.el" compile-angel-excluded-files)
+  (push "/post-early-init.el" compile-angel-excluded-files)
+  (compile-angel-on-load-mode 1))
+
+;; Core Emacs settings
+(use-package emacs
+  :init
+  (delete-selection-mode 1)
+  (global-hl-line-mode 1)
+  :hook
+  (after-init . display-time-mode)
+  (after-init . show-paren-mode)
+  (after-init . global-auto-revert-mode)
+  (after-init . recentf-mode)
+  (after-init . save-place-mode)
+  (after-init . savehist-mode)
+  :custom
+  (text-mode-ispell-word-completion nil)
+  :config
+  (setq-default display-line-numbers-type 'relative)
+  (setq package-install-upgrade-built-in t)
+  (dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
+    (add-hook hook #'display-line-numbers-mode))
+  (set-face-attribute 'default nil
+                      :height 200 :weight 'normal :family "Berkeley Mono")
+  :bind (("C-6" . (lambda () (interactive) (switch-to-buffer (other-buffer))))))
+
+;; Themes
+(use-package ef-themes
+  :ensure t
+  :demand t
+  :init
+  (load-theme 'ef-melissa-light t))
+
+;; Completion system
+(use-package marginalia
+  :ensure t
+  :init
+  (marginalia-mode))
+
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode)
+  :bind (:map vertico-map
+              ("C-j" . vertico-next)
+              ("C-k" . vertico-previous)
+              ("C-w" . vertico-directory-delete-word)
+              ("RET" . vertico-directory-enter)))
+
+(use-package orderless
+  :ensure t
+  :custom
+  (orderless-style-dispatchers '(orderless-affix-dispatch))
+  (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-category-defaults nil))
+
+(use-package corfu
+  :ensure t
+  :custom
+  (corfu-cycle t)
+  (corfu-preselect nil)
+  (corfu-auto nil)
+  :bind (:map corfu-map
+              ("C-n" . corfu-next)
+              ("C-p" . corfu-previous)
+              ("<escape>" . corfu-quit)
+              ("C-g" . corfu-quit)
+              ("C-e" . corfu-quit)
+              ("RET" . corfu-insert)
+              ("C-y" . corfu-insert)
+              ("M-d" . corfu-show-documentation)
+              ("M-l" . corfu-show-location))
+  :init
+  (global-corfu-mode)
+  (corfu-popupinfo-mode)
+  :config
+  (defun corfu-enable-always-in-minibuffer () ;; https://github.com/minad/corfu#completing-with-corfu-in-the-minibuffer
+    "Enable Corfu in the minibuffer if Vertico/Mct are not active."
+    (unless (or (bound-and-true-p mct--active)
+                (bound-and-true-p vertico--input))
+      (setq-local corfu-auto nil)
+      (corfu-mode 1)))
+  (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1))
+
+(use-package cape
+  :ensure t
+  :init
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-symbol))
+
+;; Version control
+(use-package magit
+  :ensure t
+  :bind (:map magit-mode-map
+              ("<tab>" . nil)
+              ("=" . magit-section-toggle)))
+
+;; Search and navigation
+(use-package consult
+  :ensure t
+  :bind (("C-c f l" . consult-line)
+         ("C-c f f" . consult-fd)
+         ("C-c f g" . consult-ripgrep)))
+
+(use-package wgrep
+  :ensure t)
+
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)
+   ("C-," . embark-dwim)
+   ("C-/" . embark-bindings))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package avy
+  :ensure t
+  :bind (("M-i" . avy-goto-char-2)))
+
+(use-package ace-window
+  :ensure t
+  :init
+  (defvar aw-dispatch-alist
+    '((?c aw-delete-window "Delete Window")
+      (?x aw-swap-window "Swap Windows")
+      (?m aw-move-window "Move Window")
+      (?n aw-flip-window "Flip Window")
+      (?s aw-split-window-vert "Split Vert Window")
+      (?v aw-split-window-horz "Split Horz Window")
+      (?o delete-other-windows "Delete Other Windows")
+      (?? aw-show-dispatch-help))
+    "List of actions for `aw-dispatch-default'.")
+  :custom
+  (aw-dispatch-always t)
+  :config
+  (setq aw-keys '(?h ?l ?j ?k))
+  :bind (("M-o" . ace-window)))
+
+;; Language support
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (setq treesit-font-lock-level 4)
+  (global-treesit-auto-mode))
 
 (use-package lsp-mode
   :ensure t
@@ -28,7 +193,7 @@
   ;; Setup lsp to use corfu for lsp completion
   (defun crnvl96/corfu-setup-lsp ()
     "Use orderless completion style with lsp-capf instead of the
-    default lsp-passthrough."
+               default lsp-passthrough."
     (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
           '(orderless))))
 
@@ -37,6 +202,18 @@
 
 (use-package go-mode
   :ensure t)
+
+(use-package markdown-mode
+  :commands (gfm-mode
+             gfm-view-mode
+             markdown-mode
+             markdown-view-mode)
+  :mode (("\\.markdown\\'" . markdown-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("README\\.md\\'" . gfm-mode))
+  :bind
+  (:map markdown-mode-map
+        ("C-c C-e" . markdown-do)))
 
 (use-package apheleia
   :ensure t
@@ -61,6 +238,7 @@
   :custom
   (lsp-ui-doc-show-with-mouse nil))
 
+;; Utilities
 (use-package which-key
   :ensure nil
   :commands which-key-mode
@@ -74,45 +252,6 @@
   (which-key-add-key-based-replacements
     "C-x p" "Project"
     "C-c ." "LSP"))
-
-;; The markdown-mode package provides a major mode for Emacs for syntax
-;; highlighting, editing commands, and preview support for Markdown documents.
-;; It supports core Markdown syntax as well as extensions like GitHub Flavored
-;; Markdown (GFM).
-(use-package markdown-mode
-  :commands (gfm-mode
-             gfm-view-mode
-             markdown-mode
-             markdown-view-mode)
-  :mode (("\\.markdown\\'" . markdown-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("README\\.md\\'" . gfm-mode))
-  :bind
-  (:map markdown-mode-map
-        ("C-c C-e" . markdown-do)))
-
-(use-package avy
-  :ensure t
-  :bind (("M-i" . avy-goto-char-2)))
-
-(use-package ace-window
-  :ensure t
-  :init
-  (defvar aw-dispatch-alist
-    '((?c aw-delete-window "Delete Window")
-  	  (?x aw-swap-window "Swap Windows")
-  	  (?m aw-move-window "Move Window")
-  	  (?n aw-flip-window "Flip Window")
-  	  (?s aw-split-window-vert "Split Vert Window")
-  	  (?v aw-split-window-horz "Split Horz Window")
-  	  (?o delete-other-windows "Delete Other Windows")
-  	  (?? aw-show-dispatch-help))
-    "List of actions for `aw-dispatch-default'.")
-  :custom
-  (aw-dispatch-always t)
-  :config
-  (setq aw-keys '(?f ?j ?d ?k ?l ?h ?g ?h ?a))
-  :bind (("M-o" . ace-window)))
 
 (use-package exec-path-from-shell
   :ensure t
@@ -171,4 +310,4 @@
 ;; byte-compile-warnings: (not obsolete free-vars)
 ;; End:
 
-;;; post-init.el ends her
+;;; post-init.el ends here
