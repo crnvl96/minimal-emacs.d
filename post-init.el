9a9322aaffe630a8172174d878b2439abe4bf868
Adran Carnavale <adran@hey.com>
2025-11-07 14:40:22 -0300

    updates

--- post-init.el
+++ post-init.el
@@ -1,10 +1,7 @@
 ;;; post-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
 
-(load-file "~/.emacs.d/user/options.el")
+(load-file "~/.emacs.d/user/builtin.el")
 (load-file "~/.emacs.d/user/themes.el")
-(load-file "~/.emacs.d/user/treesitter.el")
-(load-file "~/.emacs.d/user/lsp.el")
 (load-file "~/.emacs.d/user/keymaps.el")
-(load-file "~/.emacs.d/user/utilities.el")
-(load-file "~/.emacs.d/user/completion.el")
+(load-file "~/.emacs.d/user/packages.el")
 (load-file "~/.emacs.d/user/lang.el")
