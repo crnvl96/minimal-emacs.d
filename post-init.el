eab09ae8d480d06cce894f4ae2d106109871d751
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-09 00:31:45 -0300

    Qol updates

--- post-init.el
+++ post-init.el
@@ -1,8 +1,22 @@
 ;;; post-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
 
+
 (use-package emacs
   :ensure nil
+  :hook
+  (after-init . auto-save-visited-mode)
+  (after-init . tooltip-mode)
+  (after-init . delete-selection-mode)
+  (after-init . global-hl-line-mode)
+  (after-init . display-time-mode)
+  (after-init . show-paren-mode)
+  (after-init . winner-mode)
+  (after-init . minibuffer-depth-indicate-mode)
+  ((prog-mode text-mode conf-mode) . display-line-numbers-mode)
+  (dired-mode . dired-hide-details-mode)
   :custom
+  ;; This setting can be used together with `use-package-report' to profile emacs startup time
+  ;; (use-package-compute-statistics t)
   (auto-save-visited-interval 5)
   (scroll-margin 8)
   (hscroll-margin 16)
@@ -11,49 +25,36 @@
   (tooltip-delay 0.4)
   (tooltip-short-delay 0.08)
   (package-install-upgrade-built-in t)
-  :init
-  (auto-save-visited-mode 1)
-  (tooltip-mode 1)
-  (delete-selection-mode 1)
-  (global-hl-line-mode 1)
-  :hook
-  (after-init . display-time-mode)
-  (after-init . show-paren-mode)
-  (after-init . winner-mode)
-  (dired-mode . dired-hide-details-mode)
-  (after-init . minibuffer-depth-indicate-mode)
-  ((prog-mode text-mode conf-mode) . display-line-numbers-mode)
   :config
   (setq-default display-line-numbers-type 'relative)
   (set-face-attribute 'default nil :height 200 :weight 'normal :family "Berkeley Mono"))
 
 (use-package eww
-  :ensure nil)
+  :ensure nil
+  :commands eww)
 
 (use-package whitespace
   :ensure nil
+  :hook
+  (after-init . global-whitespace-mode)
   :custom
   (whitespace-style '(face trailing empty))
-  (whitespace-highlight-on-current-line t)
-  :init
-  (global-whitespace-mode t))
+  (whitespace-highlight-on-current-line t))
 
 (use-package savehist
   :ensure nil
-  :commands (savehist-mode savehist-save)
   :hook
   (after-init . savehist-mode)
   :custom
   (savehist-autosave-interval 600)
   (savehist-additional-variables
    '(kill-ring
-     register-alist
-     mark-ring global-mark-ring
-     search-ring regexp-search-ring)))
+	 register-alist
+	 mark-ring global-mark-ring
+	 search-ring regexp-search-ring)))
 
 (use-package autorevert
   :ensure nil
-  :commands (auto-revert-mode global-auto-revert-mode)
   :hook
   (after-init . global-auto-revert-mode)
   :custom
@@ -65,28 +66,22 @@
 
 (use-package recentf
   :ensure nil
-  :commands (recentf-mode recentf-cleanup)
   :hook
   (after-init . recentf-mode)
   :custom
   (recentf-auto-cleanup (if (daemonp) 300 'never))
   (recentf-exclude
    (list "\\.tar$" "\\.tbz2$" "\\.tbz$" "\\.tgz$" "\\.bz2$"
-         "\\.bz$" "\\.gz$" "\\.gzip$" "\\.xz$" "\\.zip$"
-         "\\.7z$" "\\.rar$"
-         "COMMIT_EDITMSG\\'"
-         "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\|bmp\\|xpm\\)$"
-         "-autoloads\\.el$" "autoload\\.el$"))
+		 "\\.bz$" "\\.gz$" "\\.gzip$" "\\.xz$" "\\.zip$"
+		 "\\.7z$" "\\.rar$"
+		 "COMMIT_EDITMSG\\'"
+		 "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\|bmp\\|xpm\\)$"
+		 "-autoloads\\.el$" "autoload\\.el$"))
   :config
-  ;; A cleanup depth of -90 ensures that `recentf-cleanup' runs before
-  ;; `recentf-save-list', allowing stale entries to be removed before the list
-  ;; is saved by `recentf-save-list', which is automatically added to
-  ;; `kill-emacs-hook' by `recentf-mode'.
   (add-hook 'kill-emacs-hook #'recentf-cleanup -90))
 
 (use-package saveplace
   :ensure nil
-  :commands (save-place-mode save-place-local-mode)
   :hook
   (after-init . save-place-mode)
   :custom
@@ -101,48 +96,14 @@
 
 (use-package server
   :ensure nil
-  :commands server-start
   :hook
   (after-init . server-start))
 
-(use-package modus-themes
-  :ensure t
-  :demand t
-  :init
-  (modus-themes-include-derivatives-mode 1)
-  :config
-  (setq modus-themes-mixed-fonts t
-        modus-themes-variable-pitch-ui t
-        modus-themes-italic-constructs t
-        modus-themes-bold-constructs t
-        modus-themes-completions '((t . (bold)))
-        modus-themes-prompts '(bold)
-        modus-themes-headings
-        '((agenda-structure . (variable-pitch light 2.2))
-          (agenda-date . (variable-pitch regular 1.3))
-          (t . (regular 1.15))))
-  (setq modus-themes-common-palette-overrides nil))
-
 (use-package ef-themes
-  :ensure t)
-
-(use-package standard-themes
-  :ensure t)
-
-(use-package doric-themes
-  :ensure t)
-
-(use-package doom-themes
   :ensure t
-  :custom
-  (doom-themes-enable-bold t)
-  (doom-themes-enable-italic t)
   :config
-  (doom-themes-visual-bell-config)
-  (doom-themes-org-config))
-
-(mapc #'disable-theme custom-enabled-themes)
-(load-theme 'doom-city-lights t)
+  (mapc #'disable-theme custom-enabled-themes)
+  (load-theme 'ef-dark t))
 
 (use-package undo-fu
   :ensure t
@@ -150,18 +111,16 @@
 
 (use-package undo-fu-session
   :ensure t
-  :commands undo-fu-session-global-mode
   :hook (after-init . undo-fu-session-global-mode))
 
 (use-package evil
   :ensure t
-  :commands (evil-mode evil-define-key)
-  :hook (after-init . evil-mode)
   :init
   (setq evil-undo-system 'undo-fu)
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
+  :hook (after-init . evil-mode)
   :custom
   (evil-ex-visual-char-range t)
   (evil-ex-search-vim-style-regexp t)
@@ -178,79 +137,75 @@
   (evil-want-Y-yank-to-eol t))
 
 (use-package evil-collection
-  :after evil
   :ensure t
+  :after evil
   :init
   (setq evil-collection-setup-minibuffer t)
   :config
   (evil-collection-init))
 
+(with-eval-after-load "evil"
+  (evil-define-operator crnvl96/evil-comment-or-uncomment (beg end)
+    "Toggle comment for the region between BEG and END."
+    (interactive "<r>")
+    (comment-or-uncomment-region beg end))
+  (evil-define-key 'normal 'global (kbd "gc") 'crnvl96/evil-comment-or-uncomment))
+
 (use-package magit
-  :ensure t)
+  :ensure t
+  :commands magit
+  :bind ("C-x g" . magit))
 
 (use-package eat
-  :ensure t)
+  :ensure t
+  :commands eat)
 
 (use-package wgrep
   :ensure t)
 
-(use-package persist-text-scale
-  :ensure t
-  :commands (persist-text-scale-mode
-             persist-text-scale-restore)
-  :hook (after-init . persist-text-scale-mode)
-  :custom
-  (text-scale-mode-step 1.07))
-
-(use-package aggressive-indent
-  :ensure t
-  :commands aggressive-indent-mode
-  :hook
-  (emacs-lisp-mode . aggressive-indent-mode))
-
 (use-package elisp-refs
   :ensure t
   :commands (elisp-refs-function
-             elisp-refs-macro
-             elisp-refs-variable
-             elisp-refs-special
-             elisp-refs-symbol))
+			 elisp-refs-macro
+			 elisp-refs-variable
+			 elisp-refs-special
+			 elisp-refs-symbol))
 
 (use-package buffer-terminator
   :ensure t
+  :hook
+  (after-init . buffer-terminator-mode)
   :custom
   (buffer-terminator-verbose nil)
   (buffer-terminator-inactivity-timeout (* 20 60))
-  (buffer-terminator-interval (* 10 60))
-  :config
-  (buffer-terminator-mode 1))
+  (buffer-terminator-interval (* 10 60)))
 
 (use-package helpful
   :ensure t
   :commands (helpful-callable
-             helpful-variable
-             helpful-key
-             helpful-command
-             helpful-at-point
-             helpful-function)
+			 helpful-variable
+			 helpful-key
+			 helpful-command
+			 helpful-at-point
+			 helpful-function)
+  :custom
+  (helpful-max-buffers 3)
   :bind
   ([remap describe-command] . helpful-command)
   ([remap describe-function] . helpful-callable)
   ([remap describe-key] . helpful-key)
   ([remap describe-symbol] . helpful-symbol)
-  ([remap describe-variable] . helpful-variable)
-  :custom
-  (helpful-max-buffers 3))
+  ([remap describe-variable] . helpful-variable))
 
 (use-package marginalia
   :ensure t
-  :init
-  (marginalia-mode))
+  :hook
+  (after-init . marginalia-mode))
 
 (use-package vertico
   :ensure t
-  :init
-  (vertico-mode))
+  :hook
+  (after-init . vertico-mode))
 
 (use-package orderless
   :ensure t
@@ -259,8 +214,20 @@
   (completion-category-overrides '((file (styles partial-completion))))
   (completion-category-defaults nil))
 
+(defun crnvl96/corfu-enable-always-in-minibuffer ()
+  "Enable Corfu in the minibuffer if Vertico/Mct are not active."
+  (unless (or (bound-and-true-p mct--active)
+	          (bound-and-true-p vertico--input))
+    (setq-local corfu-auto nil)
+    (corfu-mode 1)))
+
 (use-package corfu
   :ensure t
+  :hook
+  (after-init . global-corfu-mode)
+  (after-init . corfu-history-mode)
+  (after-init . corfu-popupinfo-mode)
+  (minibuffer-setup . crnvl96/corfu-enable-always-in-minibuffer)
   :custom
   (corfu-cycle t)
   (corfu-preselect nil)
@@ -273,21 +240,9 @@
   (text-mode-ispell-word-completion nil)
   (tab-always-indent 'complete)
   :bind (:map corfu-map
-              ("C-e" . corfu-quit)
-              ("C-i" . corfu-complete)
-              ("C-y" . corfu-insert))
-  :init
-  (global-corfu-mode)
-  (corfu-history-mode)
-  (corfu-popupinfo-mode)
-  :config
-  (defun corfu-enable-always-in-minibuffer ()
-    "Enable Corfu in the minibuffer if Vertico/Mct are not active."
-    (unless (or (bound-and-true-p mct--active)
-                (bound-and-true-p vertico--input))
-      (setq-local corfu-auto nil)
-      (corfu-mode 1)))
-  (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1))
+			  ("C-e" . corfu-quit)
+			  ("C-i" . corfu-complete)
+			  ("C-y" . corfu-insert)))
 
 (use-package cape
   :ensure t
@@ -301,14 +256,14 @@
   :init
   (setq prefix-help-command #'embark-prefix-help-command)
   (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
-  :bind (("C-." . embark-act)
-	     ("C-," . embark-dwin)
-	     ("C-/" . embark-bindingsk))
   :config
   (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
+			   '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+			     nil
+			     (window-parameters (mode-line-format . none))))
+  :bind (("C-." . embark-act)
+		 ("C-," . embark-dwin)
+		 ("C-/" . embark-bindingsk)))
 
 (use-package embark-consult
   :ensure t
@@ -316,39 +271,44 @@
   (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package consult
-  :ensure t)
+  :ensure t
+  :bind (("C-c f f" . consult-fd)
+		 ("C-c f l" . consult-line)
+		 ("C-c f g" . consult-ripgrep)
+		 ("C-c f b" . consult-project-buffer)
+		 ("C-c f L" . consult-goto-line)))
 
 (setq treesit-language-source-alist
-      ;; `crnvl96/treesit-install-all-languages' to install all languages
-      ;; `treesit-install-language-grammar' to install a specific language
-      ;; use `sort-lines' to sort
+      ;; - Use `crnvl96/treesit-install-all-languages' to install all languages
+      ;; - Use `treesit-install-language-grammar' to install a specific language
+      ;; - Use `sort-lines' to sort
       '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
-        (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
-        (go "https://github.com/tree-sitter/tree-sitter-go")
-        (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
-        (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.23.0"))
-        (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
-        (jsdoc "https://github.com/tree-sitter/tree-sitter-jsdoc")
-        (json "https://github.com/tree-sitter/tree-sitter-json")
-        (markdown . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
-        (markdown-inline . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown-inline/src"))
-        (mermaid "https://github.com/monaqa/tree-sitter-mermaid")
-        (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-        (rust "https://github.com/tree-sitter/tree-sitter-rust")
-        (toml "https://github.com/ikatyang/tree-sitter-toml")
-        (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
-        (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
-        (typst "https://github.com/uben0/tree-sitter-typst")
-        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+	    (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
+	    (go "https://github.com/tree-sitter/tree-sitter-go")
+	    (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
+	    (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.23.0"))
+	    (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
+	    (jsdoc "https://github.com/tree-sitter/tree-sitter-jsdoc")
+	    (json "https://github.com/tree-sitter/tree-sitter-json")
+	    (markdown . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
+	    (markdown-inline . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown-inline/src"))
+	    (mermaid "https://github.com/monaqa/tree-sitter-mermaid")
+	    (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+	    (rust "https://github.com/tree-sitter/tree-sitter-rust")
+	    (toml "https://github.com/ikatyang/tree-sitter-toml")
+	    (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
+	    (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
+	    (typst "https://github.com/uben0/tree-sitter-typst")
+	    (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
 (defun crnvl96/treesit-install-all-languages ()
   "Install all languages specified by `treesit-language-source-alist'."
   (interactive)
   (let ((languages (mapcar 'car treesit-language-source-alist)))
     (dolist (lang languages)
-	  (treesit-install-language-grammar lang)
-	  (message "`%s' parser was installed." lang)
-	  (sit-for 0.75))))
+      (treesit-install-language-grammar lang)
+      (message "`%s' parser was installed." lang)
+      (sit-for 0.75))))
 
 (add-to-list 'major-mode-remap-alist '(js-json-mode . json-ts-mode))
 (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
@@ -358,79 +318,79 @@
 (add-to-list 'auto-mode-alist '("\\.m?js\\'" . js-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
 
+(defun crnvl96/corfu-setup-lsp ()
+  "Use orderless and corfu completion style with lsp-capf instead of the
+  default lsp-passthrough."
+  (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+	    '(orderless)))
+
 (use-package lsp-mode
   :ensure t
-  :init
-  (setq lsp-keymap-prefix "C-c .")
-  (setq lsp-headerline-breadcrumb-enable nil)
-  (setq lsp-semantic-tokens-enable nil)
-  (setq lsp-enable-symbol-highlighting nil)
-  :hook ((python-ts-mode . lsp)
-         (go-ts-mode . lsp)
-         (lsp-mode . lsp-ui-mode)
-         (lsp-completion-mode . crnvl96/corfu-setup-lsp))
   :commands lsp
+  :hook ((go-ts-mode . lsp-deferred)
+		 (lsp-mode . lsp-ui-mode)
+		 (lsp-completion-mode . crnvl96/corfu-setup-lsp))
   :custom
   (lsp-completion-provider :none)
-  :config
-  (defun crnvl96/corfu-setup-lsp ()
-    "Use orderless and corfu completion style with lsp-capf instead of the
-               default lsp-passthrough."
-    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
-          '(orderless))))
+  (lsp-keymap-prefix "C-c .")
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-semantic-tokens-enable nil)
+  (lsp-enable-symbol-highlighting nil))
 
 (use-package lsp-ui
   :ensure t
-  :commands lsp-ui-mode
+  :defer t
   :custom
   (lsp-ui-doc-show-with-mouse nil))
 
 (use-package lsp-pyright
-  :ensure t)
+  :ensure t
+  :defer t
+  :hook (python-ts-mode . (lambda ()
+                            (require 'lsp-pyright)
+                            (lsp-deferred)))
+  :custom (lsp-pyright-langserver-command "pyright"))
 
 (use-package apheleia
   :ensure t
-  :commands (apheleia-mode apheleia-global-mode)
   :hook ((prog-mode . apheleia-mode))
   :config
   (setf (alist-get 'python-ts-mode apheleia-mode-alist)
-        '(ruff-isort ruff))
+		'(ruff-isort ruff))
   (setf (alist-get 'go-ts-mode apheleia-mode-alist)
-        '(gofmt)))
+		'(gofmt)))
 
 (use-package flymake-ruff
   :ensure t
   :hook (python-ts-mode . flymake-ruff-load))
 
-(use-package pyvenv
-  :ensure t)
-
 (use-package go-mode
-  :ensure t)
+  :ensure t
+  :defer t)
 
 (use-package markdown-mode
   :ensure t
   :commands (gfm-mode
-             gfm-view-mode
-             markdown-mode
-             markdown-view-mode)
+			 gfm-view-mode
+			 markdown-mode
+			 markdown-view-mode)
   :mode (("\\.markdown\\'" . markdown-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("README\\.md\\'" . gfm-mode)))
+		 ("\\.md\\'" . markdown-mode)
+		 ("README\\.md\\'" . gfm-mode)))
 
 (use-package typst-ts-mode
   :ensure t
-  :init
-  (add-to-list 'auto-mode-alist '("\\.typ\\'" . typst-ts-mode))
+  :mode (("\\.typ\\'" . typst-ts-mode))
   :custom
   (typst-ts-watch-options "--open")
-  (typst-ts-mode-enable-raw-blocks-highlight t))
+  (typst-ts-mode-enable-raw-blocks-highlight t)
+  :bind (:map typst-ts-mode-map
+			  ("C-c C-c" . typst-ts-menu)))
 
 (use-package org
   :ensure t
   :commands (org-mode org-version)
-  :mode
-  ("\\.org\\'" . org-mode)
+  :mode (("\\.org\\'" . org-mode))
   :custom
   (org-hide-leading-stars t)
   (org-startup-indented t)
@@ -444,21 +404,10 @@
 
 (use-package which-key
   :ensure nil
-  :demand t
   :commands which-key-mode
   :hook (after-init . which-key-mode)
   :config
   (which-key-add-key-based-replacements
-    "C-x p" "Project"
-    "C-c ." "LSP"
-    "C-c f" "Find")
-  (which-key-add-keymap-based-replacements global-map
-    ;; Consult
-    "C-c f f" '("Find files" . consult-fd)
-    "C-c f l" '("Find lines" . consult-line)
-    "C-c f g" '("Grep files" . consult-ripgrep)
-    "C-c f b" '("List buffers" . consult-project-buffer)
-    "C-c f L" '("Go to line" . consult-goto-line))
-  (which-key-add-keymap-based-replacements typst-ts-mode-map
-    ;; Typst-ts-mode
-    "C-c C-c" '("Typst menu" . typst-ts-tmenu)))
+	"C-x p" "Project"
+	"C-c ." "LSP"
+	"C-c f" "Find"))
