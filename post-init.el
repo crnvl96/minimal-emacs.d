2205c80c757d63f9f6982e782ef97d5623533666
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-11 15:17:05 -0300

    Updates

--- post-init.el
+++ post-init.el
@@ -1,12 +1,16 @@
 ;;; post-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
 
+;; Delight is a package that modify the lighter value displayed in the mode line for
+;; the given mode spec.
+;; It is used alongside the configuration file inside use-package declarations, to declutter the information
+;; showed on mode-line
 (use-package delight
   :ensure t)
 
+;; Theme package
 (use-package modus-themes
   :ensure t
   :custom
-  (modus-themes-to-rotate modus-themes-items)
   (modus-themes-mixed-fonts t)
   (modus-themes-variable-pitch-ui t)
   (modus-themes-italic-constructs t)
@@ -14,53 +18,68 @@
   (modus-themes-completions '((t . (bold))))
   (modus-themes-prompts '(bold))
   (modus-themes-headings)
-  '((agenda-structure . (variable-pitch light 1.2))
-    (agenda-date . (variable-pitch regular 1.3))
-    (t . (regular 1.15)))
-  (setq modus-themes-common-palette-overrides nil))
-
-(use-package doric-themes
-  :ensure t)
-
-(use-package standard-themes
-  :ensure t)
-
-(use-package ef-themes
-  :ensure t
   :config
   (mapc #'disable-theme custom-enabled-themes)
-  (load-theme 'ef-elea-dark t))
+  (load-theme 'modus-operandi-tinted t))
 
 (use-package all-the-icons
   :if (display-graphic-p))
 
 (use-package emacs
   :ensure nil
-  :delight
-  (eldoc-mode)
-  (emacs-lisp-mode "Elisp" :major)
-  :hook
-  (after-init . global-display-line-numbers-mode)
-  (after-init . display-time-mode)
-  (after-init . global-hl-line-mode)
-  (after-init . winner-mode)
-  (after-init . savehist-mode)
-  (after-init . show-paren-mode)
-  (after-init . delete-selection-mode)
   :custom
   (package-install-upgrade-built-in t)
+  (completion-ignore-case t)
+  (compile-command nil)
   :config
+  ;; Fonts definition ---
   (set-face-attribute 'default nil :height 220 :weight 'normal :family "Iosevka")
   (set-face-attribute 'variable-pitch nil :height 220 :weight 'normal :family "Iosevka Aile")
-  (with-eval-after-load "org-mode"
-    (set-face-attribute 'org-modern-symbol nil :height 220 :weight 'normal :family "Iosevka"))
+
+  ;; Add a more "Fluid" mouse scroll animation ---
   (unless (and (eq window-system 'mac)
                (bound-and-true-p mac-carbon-version-string))
     (setq pixel-scroll-precision-use-momentum nil)
     (pixel-scroll-precision-mode 1))
   :bind
   ("M-n" . forward-paragraph)
-  ("M-p" . backward-paragraph))
+  ("M-p" . backward-paragraph)
+  ("C-x ;" . comment-or-uncomment-region))
+
+(use-package savehist
+  :ensure nil
+  :hook
+  (after-init . savehist-mode))
+
+(use-package paren
+  :ensure nil
+  :hook
+  (after-init . show-paren-mode))
+
+(use-package display-line-numbers
+  :ensure nil
+  :hook (after-init . global-display-line-numbers-mode))
+
+(use-package time
+  :ensure nil
+  :hook (after-init . display-time-mode))
+
+(use-package hl-line
+  :ensure nil
+  :hook (after-init . global-hl-line-mode))
+
+(use-package delsel
+  :ensure nil
+  :hook (after-init . delete-selection-mode))
+
+(use-package winner
+  :ensure nil
+  :hook
+  (after-init . winner-mode))
+
+(use-package eldoc
+  :ensure nil
+  :delight)
 
 (use-package autorevert
   :ensure nil
@@ -94,6 +113,11 @@
   :custom
   (save-place-limit 150))
 
+(use-package flymake
+  :ensure nil
+  :bind (("C-c . ]" . flymake-goto-next-error)
+         ("C-c . [" . flymake-goto-prev-error)))
+
 (use-package which-key
   :ensure nil
   :delight
@@ -111,42 +135,42 @@
   :ensure nil
   :config
   (setq treesit-language-source-alist
-        ;; - Use `crnvl96/treesit-install-all-languages' to install all languages
+        ;; - Use `my/treesit-install-all-languages' to install all languages
         ;; - Use `treesit-install-language-grammar' to install a specific language
-        '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
-	      (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
-	      (go "https://github.com/tree-sitter/tree-sitter-go")
-	      (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
-	      (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.23.0"))
-	      (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
-	      (jsdoc "https://github.com/tree-sitter/tree-sitter-jsdoc")
-	      (json "https://github.com/tree-sitter/tree-sitter-json")
-	      (markdown . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
-	      (markdown-inline . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown-inline/src"))
-	      (mermaid "https://github.com/monaqa/tree-sitter-mermaid")
-	      (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-	      (rust "https://github.com/tree-sitter/tree-sitter-rust")
-	      (toml "https://github.com/ikatyang/tree-sitter-toml")
-	      (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
-	      (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
-	      (typst "https://github.com/uben0/tree-sitter-typst")
-	      (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
+          (jsdoc "https://github.com/tree-sitter/tree-sitter-jsdoc")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown")
+          (markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown")
+          (mermaid "https://github.com/monaqa/tree-sitter-mermaid")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust")
+          (toml "https://github.com/ikatyang/tree-sitter-toml")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript")
+          (typescript "https://github.com/tree-sitter/tree-sitter-typescript")
+          (typst "https://github.com/uben0/tree-sitter-typst")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
-  (defun crnvl96/treesit-install-all-languages ()
+  (defun my/treesit-install-all-languages ()
     "Install all languages specified by `treesit-language-source-alist'."
     (interactive)
     (let ((languages (mapcar 'car treesit-language-source-alist)))
       (dolist (lang languages)
         (treesit-install-language-grammar lang)
         (message "`%s' parser was installed." lang)
-        (sit-for 0.75))))
+        (sit-for 0.75)))))
 
-  (add-to-list 'major-mode-remap-alist '(js-json-mode . json-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(go-mode . go-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.m?js\\'" . js-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode)))
+(add-to-list 'major-mode-remap-alist '(js-json-mode . json-ts-mode))
+(add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
+(add-to-list 'major-mode-remap-alist '(go-mode . go-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.m?js\\'" . js-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
 
 (use-package ace-window
   :ensure t
@@ -166,23 +190,21 @@
   :bind (("C-=" . er/expand-region)))
 
 (use-package crux
-  :ensure t
+  :demand t
   :bind (([remap move-beginning-of-line] . crux-move-beginning-of-line)
          ([remap kill-whole-line] . crux-kill-whole-line)
          ([remap keyboard-quit] . crux-keyboard-quit-dwin)
          ([remap upcase-region] . crux-upcase-region)
          ([remap downcase-region] . crux-downcase-region)
-         ("C-^" . crux-top-join-line)
-         ("C-6" . crux-other-window-or-switch-buffer)
+         ("C-S-j" . crux-top-join-line)
          ("C-k" . crux-smart-kill-line)
          ("S-<return>" . crux-smart-open-line)
          ("C-S-<return>" . crux-smart-open-line-above)
-         ("C-c c x" . crux-transpose-windows)
          ("C-c c o" . crux-open-with)
+         ("C-c c u" . crux-view-url)
+         ("C-c c t" . crux-visit-term-buffer)
          ("C-c c d" . crux-duplicate-current-line-or-region)
          ("C-c c n" . crux-cleanup-buffer-or-region)
-         ("C-c c r" . crux-recentf-find-file)
-         ("C-c c R" . crux-recentf-find-directory)
          ("C-c c k" . crux-kill-other-buffers))
   :config
   (crux-with-region-or-line comment-or-uncomment-region)
@@ -197,14 +219,20 @@
 (use-package vertico
   :ensure t
   :hook
-  (after-init . vertico-mode))
+  (after-init . vertico-mode)
+  :bind (:map vertico-map
+              ("<backspace>" . vertico-directory-delete-char)
+              ("C-j" . vertico-next)
+              ("C-k" . vertico-previous)
+              ("C-w" . vertico-directory-delete-word )
+              ("RET" . vertico-directory-enter)))
 
 (use-package orderless
   :ensure t
   :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles partial-completion))))
-  (completion-category-defaults nil))
+  (completion-styles '(orderless partial-completion basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides nil))
 
 (use-package corfu
   :ensure t
@@ -212,18 +240,30 @@
   (after-init . global-corfu-mode)
   (after-init . corfu-history-mode)
   (after-init . corfu-popupinfo-mode)
-  (minibuffer-setup . crnvl96/corfu-enable-always-in-minibuffer)
+  (minibuffer-setup . my/corfu-enable-always-in-minibuffer)
   :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.1)
+  (corfu-popupinfo-delay '(0.5 . 0.5))
   (read-extended-command-predicate #'command-completion-default-include-p)
   (text-mode-ispell-word-completion nil)
   (tab-always-indent 'complete)
   :bind (:map corfu-map
-              ("RET" . nil)
               ("C-e" . corfu-quit)
               ("C-i" . corfu-complete)
-              ("C-y" . corfu-insert))
+              ("C-j" . corfu-next)
+              ("M-j" . corfu-info-documentation)
+              ("C-k" . corfu-previous)
+              ("M-k" . corfu-info-location)
+              ("C-y" . corfu-insert)
+              ("C-n" . corfu-quit)
+              ("C-p" . corfu-quit)
+              ("RET" . corfu-quit))
   :config
-  (defun crnvl96/corfu-enable-always-in-minibuffer ()
+  (keymap-unset corfu-map "RET")
+  (defun my/corfu-enable-always-in-minibuffer ()
     "Enable Corfu in the minibuffer if Vertico/Mct are not active."
     (unless (or (bound-and-true-p mct--active)
                 (bound-and-true-p vertico--input))
@@ -237,13 +277,13 @@
   (add-to-list 'completion-at-point-functions #'cape-elisp-block)
   (add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
   (add-to-list 'completion-at-point-functions #'cape-keyword)
-  (add-hook 'completion-at-point-functions #'cape-dabbrev))
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev))
 
 (use-package embark
   :ensure t
-  :init
-  (setq prefix-help-command #'embark-prefix-help-command)
-  (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+  :custom
+  (prefix-help-command #'embark-prefix-help-command)
+  (eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
   :config
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
@@ -255,17 +295,31 @@
 
 (use-package embark-consult
   :ensure t
+  :after (embark consult)
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package consult
   :ensure t
+  :custom
+  (consult-async-min-input 2)
   :bind (("C-c b" . consult-buffer)
+         ("C-c y" . consult-yank-pop)
          ("C-c f f" . consult-fd)
+         ("C-c f s" . consult-outline)
          ("C-c f l" . consult-line)
          ("C-c f g" . consult-ripgrep)
          ("C-c f b" . consult-project-buffer)
-         ("C-c f L" . consult-goto-line)))
+         ("C-c f L" . consult-goto-line))
+  :config
+  (defun consult--orderless-regexp-compiler (input type &rest _config)
+    (setq input (cdr (orderless-compile input)))
+    (cons
+     (mapcar (lambda (r) (consult--convert-regexp r type)) input)
+     (lambda (str) (orderless--highlight input t str))))
+
+  (setq consult--regexp-compiler #'consult--orderless-regexp-compiler))
+
 
 (use-package magit
   :ensure t)
@@ -336,7 +390,7 @@
   :commands lsp
   :hook ((go-ts-mode . lsp-deferred)
          (lsp-mode . lsp-ui-mode)
-         (lsp-completion-mode . crnvl96/corfu-setup-lsp))
+         (lsp-completion-mode . my/lsp-mode-setup-completion))
   :custom
   (lsp-completion-provider :none)
   (lsp-keymap-prefix "C-c .")
@@ -344,11 +398,13 @@
   (lsp-semantic-tokens-enable nil)
   (lsp-enable-symbol-highlighting nil)
   :config
-  (defun crnvl96/corfu-setup-lsp ()
-    "Use orderless and corfu completion style with lsp-capf instead of the
-  default lsp-passthrough."
+  (defun my/orderless-dispatch-flex-first (_pattern index _total)
+    (and (eq index 0) 'orderless-flex))
+  (defun my/lsp-mode-setup-completion ()
     (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
-          '(orderless))))
+          '(orderless))
+    (add-hook 'orderless-style-dispatchers #'my/orderless-dispatch-flex-first nil 'local)
+    (setq-local completion-at-point-functions (list (cape-capf-buster #'lsp-completion-at-point)))))
 
 (use-package lsp-ui
   :ensure t
@@ -371,6 +427,10 @@
 (use-package go-mode
   :ensure t
   :defer t)
+
+(use-package elisp-mode
+  :ensure nil
+  :delight (emacs-lisp-mode "Elisp" :major))
 
 (use-package markdown-mode
   :ensure t
@@ -399,16 +459,13 @@
   :mode (("\\.org\\'" . org-mode))
   :hook (org-mode . org-indent-mode)
   :custom
+  (org-M-RET-may-split-line '((default . nil)))
+  (org-insert-heading-respect-content t)
   (org-log-done 'time)
   (org-log-into-drawer t)
-  (org-agenda-files '("~/Developer/personal/notes/agenda/tasks.org"))
+  (org-directory "~/Developer/personal/notes/agenda/")
+  (org-agenda-files (list org-directory))
   (org-todo-keywords
    '((sequence "TODO(t)" "WAIT(w!)" "|" "CANCEL(c!)" "DONE(d!)")))
   :bind
   ("C-c a" . org-agenda))
-
-(use-package org-journal
-  :ensure t
-  :custom
-  (org-journal-dir "~/Developer/personal/notes/journal")
-  (org-journal-date-format "%A, %d %B %Y"))
