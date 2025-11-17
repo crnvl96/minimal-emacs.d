33d00ac7ecd4f48fa2b181a42dea7cbf03fabaf9
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-16 22:54:31 -0300

    Start splitting config into multiple files

---
+++ user/dired.el
@@ -0,0 +1,34 @@
+;;; dired.el -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(use-package dired
+  :ensure nil
+  :commands (dired)
+  :hook
+  ((dired-mode . dired-hide-details-mode)
+   (dired-mode . hl-line-mode))
+  :config
+  (setq dired-recursive-copies 'always)
+  (setq dired-recursive-deletes 'always)
+  (setq delete-by-moving-to-trash t)
+  (setq dired-dwim-target t))
+
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind
+  ( :map dired-mode-map
+    ("<tab>" . dired-subtree-toggle)
+    ("TAB" . dired-subtree-toggle)
+    ("<backtab>" . dired-subtree-remove)
+    ("S-TAB" . dired-subtree-remove))
+  :config
+  (setq dired-subtree-use-backgrounds nil))
+
+(use-package trashed
+  :ensure t
+  :commands (trashed)
+  :config
+  (setq trashed-action-confirmer 'y-or-n-p)
+  (setq trashed-use-header-line t)
+  (setq trashed-sort-key '("Date deleted" . t))
+  (setq trashed-date-format "%Y-%m-%d %H:%M:%S"))
---
+++ user/ui.el
@@ -0,0 +1,42 @@
+;;; ui.el -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(use-package spacious-padding
+  :ensure t
+  :hook (after-init . spacious-padding-mode)
+  :config
+  (setq spacious-padding-widths
+        '( :internal-border-width 15
+           :header-line-width 4
+           :mode-line-width 6
+           :tab-width 4
+           :right-divider-width 30
+           :scroll-bar-width 8
+           :fringe-width 8))
+
+  (setq spacious-padding-subtle-frame-lines
+        `( :mode-line-active 'default
+           :mode-line-inactive vertical-border)))
+
+(use-package modus-themes
+  :ensure t
+  :demand t
+  :init (modus-themes-include-derivatives-mode 1)
+  :config (setq modus-themes-to-toggle '(modus-operandi modus-vivendi)
+                modus-themes-to-rotate modus-themes-items
+                modus-themes-mixed-fonts t
+                modus-themes-variable-pitch-ui t
+                modus-themes-italic-constructs t
+                modus-themes-bold-constructs t
+                modus-themes-completions '((t . (bold)))
+                modus-themes-prompts '(bold)
+                modus-themes-common-palette-overrides nil
+                modus-themes-headings '((agenda-structure . (variable-pitch light 2.2))
+                                        (agenda-date . (variable-pitch regular 1.3))
+                                        (t . (regular 1.15)))))
+
+(use-package ef-themes
+  :ensure t
+  :demand t)
+
+(mapc #'disable-theme custom-enabled-themes)
+(load-theme 'ef-elea-light t)
