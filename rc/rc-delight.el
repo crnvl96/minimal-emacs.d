94586325850a69bf44369101aa659c96f9596956
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-26 11:49:04 -0300

    Let's use use-package to handle installation

--- rc/rc-delight.el
+++ rc/rc-delight.el
@@ -1,9 +1,11 @@
 ;;; -*- lexical-binding: t; -*-
 
-(require 'delight)
-
-(delight 'whitespace-mode nil "whitespace")
-(delight 'visual-line-mode nil "simple")
-(delight 'eldoc-mode nil "eldoc")
+(use-package delight
+  :ensure t
+  :demand t
+  :config
+  (delight 'whitespace-mode nil "whitespace")
+  (delight 'visual-line-mode nil "simple")
+  (delight 'eldoc-mode nil "eldoc"))
 
 (provide 'rc-delight)
