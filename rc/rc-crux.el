5d8823d2b93866f3bedefcf2481d7a427159ca5f
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-26 14:41:43 -0300

    Add some more files to the correct place

---
+++ rc/rc-crux.el
@@ -0,0 +1,26 @@
+;;; -*- lexical-binding: t; -*-
+
+(use-package crux
+  :ensure t
+  :demand t
+  :bind (([remap move-beginning-of-line] . crux-move-beginning-of-line)
+         ([remap kill-whole-line] . crux-kill-whole-line)
+         ([remap keyboard-quit]  . crux-keyboard-quit-dwin)
+         ([remap upcase-region] . crux-upcase-region)
+         ([remap downcase-region] . crux-downcase-region)
+         ("C-c c j" . crux-top-join-line)
+         ("C-c c n" . crux-cleanup-buffer-or-region)
+         ("C-c c t" . crux-visit-term-buffer)
+         ("C-c c d" . crux-duplicate-current-line-or-region)
+         ("C-k" . crux-smart-kill-line)
+         ("S-<return>" . crux-smart-open-line)
+         ("C-S-<return>" . crux-smart-open-line-above)
+         ("C-c c o" . crux-open-with)
+         ("C-c c u" . crux-view-url)
+         ("C-c c k" . crux-kill-other-buffers))
+  :config
+  (crux-with-region-or-line comment-or-uncomment-region)
+  (crux-with-region-or-sexp-or-line kill-region)
+  (crux-with-region-or-point-to-eol kill-ring-save))
+
+(provide 'rc-crux)
--- rc/rc-utilities.el
+++ rc/rc-utilities.el
@@ -16,11 +16,68 @@
   :ensure t
   :hook (after-init . editorconfig-mode))
 
+(use-package mise
+  :ensure t
+  :hook (after-init . global-mise-mode))
+
 (use-package exec-path-from-shell
   :ensure t
   :demand t
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
+
+(use-package buffer-terminator
+  :ensure t
+  :delight
+  :hook (after-init . buffer-terminator-mode)
+  :config (setq buffer-terminator-verbose nil
+                buffer-terminator-inactivity-timeout (* 20 60)
+                buffer-terminator-interval (* 20 60)))
+
+(use-package super-save
+  :ensure t
+  :delight
+  :hook (after-init . super-save-mode)
+  :init (setq auto-save-default nil)
+  :config (setq super-save-auto-save-when-idle t
+                super-save-delete-trailing-whitespace t
+                super-save-all-buffers t)
+  (add-to-list 'super-save-triggers 'ace-window)
+  (add-to-list 'super-save-hook-triggers 'find-file-hook))
+
+(use-package undo-fu
+  :demand t
+  :ensure t
+  :commands (undo-fu-only-undo
+             undo-fu-only-redo
+             undo-fu-only-redo-all
+             undo-fu-disable-checkpoint)
+  :config
+  (global-unset-key (kbd "C-z"))
+  (global-set-key (kbd "C-z") 'undo-fu-only-undo)
+  (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
+
+(use-package undo-fu-session
+  :ensure t
+  :hook (after-init . undo-fu-session-global-mode)
+  :commands (undo-fu-session-global-mode))
+
+(use-package helpful
+  :ensure t
+  :commands (helpful-callable
+             helpful-variable
+             helpful-key
+             helpful-command
+             helpful-at-point
+             helpful-function)
+  :config
+  (setq helpful-max-buffers 3)
+  :bind
+  ([remap describe-command] . helpful-command)
+  ([remap describe-function] . helpful-callable)
+  ([remap describe-key] . helpful-key)
+  ([remap describe-symbol] . helpful-symbol)
+  ([remap describe-variable] . helpful-variable))
 
 (provide 'rc-utilities)
