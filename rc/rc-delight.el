a2517b59334a13abe728e6bcb488b6d222fb034b
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-26 21:45:13 -0300

    Still trying to find what works best for me

--- rc/rc-delight.el
+++ rc/rc-delight.el
@@ -5,6 +5,8 @@
   :demand t
   :config
   (delight 'whitespace-mode nil "whitespace")
+  (delight 'which-key-mode nil "which-key")
+  (delight 'devil-mode nil "devil")
   (delight 'visual-line-mode nil "simple")
   (delight 'eldoc-mode nil "eldoc"))
 
