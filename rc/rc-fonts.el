ddfec67f3b33c684b84547397ac071281a786438
Adran Carnavale <adran.carnavale@gmail.com>
2025-11-28 14:05:07 -0300

    Increase fonts

--- rc/rc-fonts.el
+++ rc/rc-fonts.el
@@ -1,6 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
-(set-face-attribute 'default nil :height 180 :weight 'normal :family "Iosevka")
-(set-face-attribute 'variable-pitch nil :height 180 :weight 'normal :family "Iosevka Aile")
+(set-face-attribute 'default nil :height 200 :weight 'normal :family "Iosevka")
+(set-face-attribute 'variable-pitch nil :height 200 :weight 'normal :family "Iosevka Aile")
 
 (provide 'rc-fonts)
